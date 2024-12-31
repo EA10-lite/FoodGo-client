3a670f8341e767af13c425e1ec089edbc5d3840b
Emmanuel <emanuelanyigor@gmail.com>
2024-12-31 20:19:47 +0100

    updating logo

--- src/components/general/Logo.js
+++ src/components/general/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 const Logo  = () => {
     return (
         <div className="logo">
-            <img src="/img/logo.svg" className="w-[98px] h-auto object-cover" />
+            <img src="/img/foodgo-logo.svg" alt="FoodGo Logo" className="w-[98px] h-auto object-cover" />
         </div>
     )
 }
